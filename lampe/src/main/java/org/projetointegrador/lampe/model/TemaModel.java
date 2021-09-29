624b3c1b53f8e4ca50a8266c98ca1f314cededf6
Zoraten <cosmos_blood@outlook.com>
2021-09-29 11:27:47 -0300

    [Feature Guilherme: Task - Update TemaModel | 29.09.21]

--- lampe/src/main/java/org/projetointegrador/lampe/model/TemaModel.java
+++ lampe/src/main/java/org/projetointegrador/lampe/model/TemaModel.java
@@ -1,6 +1,62 @@
 package org.projetointegrador.lampe.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
 public class TemaModel {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String primarioTema;
+	
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String secundarioTema;
+	
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String eventosTema;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPrimarioTema() {
+		return primarioTema;
+	}
+
+	public void setPrimarioTema(String primarioTema) {
+		this.primarioTema = primarioTema;
+	}
+
+	public String getSecundarioTema() {
+		return secundarioTema;
+	}
+
+	public void setSecundarioTema(String secundarioTema) {
+		this.secundarioTema = secundarioTema;
+	}
+
+	public String getEventosTema() {
+		return eventosTema;
+	}
+
+	public void setEventosTema(String eventosTema) {
+		this.eventosTema = eventosTema;
+	}
 }
