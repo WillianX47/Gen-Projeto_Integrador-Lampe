80f0ee2434578a65a8d49b54e2da94084f1172b6
Eduardo <edu3839@hotmail.com>
2021-10-04 12:29:16 -0300

    [Feature Eduardo - Task: Update TemaController | 04.10.2021]

--- lampe/src/main/java/org/projetointegrador/lampe/controller/TemaController.java
+++ lampe/src/main/java/org/projetointegrador/lampe/controller/TemaController.java
@@ -1,5 +1,69 @@
 package org.projetointegrador.lampe.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.projetointegrador.lampe.model.TemaModel;
+import org.projetointegrador.lampe.repository.TemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tema")
+@CrossOrigin("*")
 public class TemaController {
+
+	private @Autowired TemaRepository repositorio;
+
+	@GetMapping
+	public ResponseEntity<List<TemaModel>> findAllTema() {
+		List<TemaModel> objetoTema = repositorio.findAll();
+		if (objetoTema.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoTema);
+		}
+	}
+
+	@PostMapping("/salvar")
+	public ResponseEntity<TemaModel> novoTema(@Valid @RequestBody TemaModel novoTema) {
+		return ResponseEntity.status(201).body(repositorio.save(novoTema));
+
+	}
+
+	@PutMapping("/atualizar")
+	public ResponseEntity<TemaModel> alterarTema(@Valid @RequestBody TemaModel alterarTema) {
+		return ResponseEntity.status(200).body(repositorio.save(alterarTema));
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<TemaModel> findAllById(@Valid @PathVariable Long id) {
+		return repositorio.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElse(ResponseEntity.status(204).build());
+	}
+
+	@DeleteMapping("/excluir/{id}")
+	public ResponseEntity<TemaModel> excluirTema(@Valid @PathVariable Long id) {
+		Optional<TemaModel> objetoOptional = repositorio.findById(id);
+		if (objetoOptional.isPresent()) {
+			repositorio.deleteById(id);
+			return ResponseEntity.status(200).build();
+		} else {
+			return ResponseEntity.status(400).build();
+		}
+
+	}
 
 }
--- lampe/src/main/java/org/projetointegrador/lampe/repository/TemaRepository.java
+++ lampe/src/main/java/org/projetointegrador/lampe/repository/TemaRepository.java
@@ -1,5 +1,10 @@
 package org.projetointegrador.lampe.repository;
 
-public class TemaRepository {
+import org.projetointegrador.lampe.model.TemaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TemaRepository extends JpaRepository<TemaModel, Long> {
 
 }
