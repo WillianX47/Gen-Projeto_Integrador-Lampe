85e2b569d8f8e33e6eec363cee15c12500615bf9
Zoraten <cosmos_blood@outlook.com>
2021-10-05 10:05:26 -0300

    [Feature Guilherme - Task: Update PostagemModel | 05.10.21]

--- lampe/src/main/java/org/projetointegrador/lampe/model/PostagemModel.java
+++ lampe/src/main/java/org/projetointegrador/lampe/model/PostagemModel.java
@@ -1,10 +1,67 @@
 package org.projetointegrador.lampe.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Entity
 @Table(name = "tb_postagem")
+@CrossOrigin("*")
 public class PostagemModel {
+
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+
+	private @NotBlank String textoPostagem;
+
+	private String anexoPostagem;
+
+	private @NotBlank String privacidade;
+
+	private String localizacaoPostagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTextoPostagem() {
+		return textoPostagem;
+	}
+
+	public void setTextoPostagem(String textoPostagem) {
+		this.textoPostagem = textoPostagem;
+	}
+
+	public String getAnexoPostagem() {
+		return anexoPostagem;
+	}
+
+	public void setAnexoPostagem(String anexoPostagem) {
+		this.anexoPostagem = anexoPostagem;
+	}
+
+	public String getPrivacidade() {
+		return privacidade;
+	}
+
+	public void setPrivacidade(String privacidade) {
+		this.privacidade = privacidade;
+	}
+
+	public String getLocalizacaoPostagem() {
+		return localizacaoPostagem;
+	}
+
+	public void setLocalizacaoPostagem(String localizacaoPostagem) {
+		this.localizacaoPostagem = localizacaoPostagem;
+	}
 
 }
