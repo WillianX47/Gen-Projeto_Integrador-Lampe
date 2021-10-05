bd588b599fcfe7e6c997c468eec588752b454708
WillianX47 <willianx47@gmail.com>
2021-10-05 09:25:06 -0300

    [Feature Will - Task: Add UsuarioModel | 05.10.21]

---
+++ lampe/src/main/java/org/projetointegrador/lampe/model/UsuarioModel.java
@@ -0,0 +1,14 @@
+package org.projetointegrador.lampe.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Entity
+@Table(name = "tb_usuario")
+@CrossOrigin("*")
+public class UsuarioModel {
+
+	
+}
