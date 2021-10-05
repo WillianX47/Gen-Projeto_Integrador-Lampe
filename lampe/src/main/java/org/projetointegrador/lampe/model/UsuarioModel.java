ce9dc429e7699a9fd70367e5e040057405a0d4f0
WillianX47 <willianx47@gmail.com>
2021-10-05 09:26:07 -0300

    [Feature Will - Task: Update UsuarioModel | 05.10.21]

--- lampe/src/main/java/org/projetointegrador/lampe/model/UsuarioModel.java
+++ lampe/src/main/java/org/projetointegrador/lampe/model/UsuarioModel.java
@@ -1,7 +1,11 @@
 package org.projetointegrador.lampe.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -10,5 +14,43 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("*")
 public class UsuarioModel {
 
+private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
 	
+	private @NotBlank String nomeUsuario;
+	
+	private @NotBlank String emailUsuario;
+	
+	private @NotBlank String senhaUsuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
 }
