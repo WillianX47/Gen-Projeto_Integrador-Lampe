c2c4f73aaf1f77fcdc5b73ef7422ad73cbc88a1d
FreitasOliver <everton.oliveira1@icloud.com>
2021-10-05 17:20:40 -0300

    [Feature Everton - Task: Add UsuarioController | 05.10.21]

---
+++ lampe/src/main/java/org/projetointegrador/lampe/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package org.projetointegrador.lampe.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("api/v1/usuario")
+@CrossOrigin ("*")
+public class UsuarioController {
+	
+
+}
