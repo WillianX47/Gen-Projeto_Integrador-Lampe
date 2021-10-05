d0cd3d0d43538f32c499d90f246e64c4170afecb
Zoraten <cosmos_blood@outlook.com>
2021-10-05 10:01:50 -0300

    [Feature Guilherme - Task: Add PostagemModel | 05.10.21]

---
+++ lampe/src/main/java/org/projetointegrador/lampe/model/PostagemModel.java
@@ -0,0 +1,10 @@
+package org.projetointegrador.lampe.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_postagem")
+public class PostagemModel {
+
+}
