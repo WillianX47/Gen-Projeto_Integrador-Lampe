2b851c94fb3ddc7da2944b4f0e8a461c07b07c41
Eduardo <edu3839@hotmail.com>
2021-10-05 12:00:07 -0300

    [Feature Eduardo - Task: Add PostagemControler | 05.10.21]

---
+++ lampe/src/main/java/org/projetointegrador/lampe/controller/PostagemController.java
@@ -0,0 +1,14 @@
+package org.projetointegrador.lampe.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/postagem")
+@RestController
+@CrossOrigin("*")
+public class PostagemController {
+	
+	
+
+}
--- lampe/src/main/java/org/projetointegrador/lampe/repository/PostagemRepository.java
+++ lampe/src/main/java/org/projetointegrador/lampe/repository/PostagemRepository.java
@@ -1,5 +1,10 @@
 package org.projetointegrador.lampe.repository;
 
-public interface PostagemRepository {
+import org.projetointegrador.lampe.model.PostagemModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+@Repository
+public interface PostagemRepository extends JpaRepository<PostagemModel, Long> {
+	
+	}
