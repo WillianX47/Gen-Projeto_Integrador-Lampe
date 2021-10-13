17efef6a8e36286730cf365ff99f186422d510ca
WillianX47 <willianx47@gmail.com>
2021-10-13 11:48:52 -0300

    [Feature Will - Task: Add BasicSecurityConfig | 13.10.21]

---
+++ lampe/src/main/java/org/projetointegrador/lampe/security/BasicSecurityConfig.java
@@ -0,0 +1,10 @@
+package org.projetointegrador.lampe.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+}
