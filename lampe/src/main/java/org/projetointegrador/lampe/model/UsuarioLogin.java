48b83a29e75725ccd82ba0e8f25145c8203a77e4
Thiago Mira Simandi <52213139+thmira@users.noreply.github.com>
2021-10-13 10:15:39 -0300

    [Feature Mira - Task: Update UsuarioLogin | 13.10.21]

--- lampe/src/main/java/org/projetointegrador/lampe/model/UsuarioLogin.java
+++ lampe/src/main/java/org/projetointegrador/lampe/model/UsuarioLogin.java
@@ -1,5 +1,55 @@
 package org.projetointegrador.lampe.model;
 
 public class UsuarioLogin {
+	
+	private String nomeUsuario;
 
+	private String emailUsuario;
+
+	private String senhaUsuario;
+
+	private String token;
+
+	private Long id;
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 }
