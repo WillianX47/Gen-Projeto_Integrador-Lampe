4f5c7b705ed27ef6a7c4c6463887256cbe6a9533
Zoraten <cosmos_blood@outlook.com>
2021-10-13 11:05:32 -0300

    [Feature Guilherme - Task: Update UsuarioService | 13.10.21]

--- lampe/src/main/java/org/projetointegrador/lampe/repository/UsuarioRepository.java
+++ lampe/src/main/java/org/projetointegrador/lampe/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package org.projetointegrador.lampe.repository;
 
+import java.util.Optional;
+
 import org.projetointegrador.lampe.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
 
+	public Optional<UsuarioModel> findByEmailUsuario (String usuario);
 }
