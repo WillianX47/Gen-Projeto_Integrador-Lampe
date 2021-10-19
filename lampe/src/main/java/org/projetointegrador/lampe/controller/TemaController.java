b6788e98e8312647b983f9e1d05e1c55fe024a79
unknown <thsimandi@gmail.com>
2021-10-19 12:58:23 -0300

    [Feature Mira - Task: Update TemaController | 19.10.2021]

--- lampe/src/main/java/org/projetointegrador/lampe/controller/TemaController.java
+++ lampe/src/main/java/org/projetointegrador/lampe/controller/TemaController.java
@@ -1,23 +1,24 @@
 package org.projetointegrador.lampe.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.projetointegrador.lampe.model.TemaModel;
 import org.projetointegrador.lampe.repository.TemaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -30,14 +31,14 @@ import io.swagger.annotations.ApiResponses;
 @CrossOrigin("*")
 public class TemaController {
 
-	private @Autowired TemaRepository repositorio;
+	private @Autowired TemaRepository repository;
 
 	@ApiOperation(value = "Procura todas os temas cadastrados no sistema")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Temas encontrados"),
 			@ApiResponse(code = 204, message = "Não existem temas no sistema") })
 	@GetMapping
 	public ResponseEntity<List<TemaModel>> findAllTema() {
-		List<TemaModel> objetoTema = repositorio.findAll();
+		List<TemaModel> objetoTema = repository.findAll();
 		if (objetoTema.isEmpty()) {
 			return ResponseEntity.status(204).build();
 		} else {
@@ -46,45 +47,42 @@ public class TemaController {
 	}
 
 	@ApiOperation(value = "Salvar um tema")
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema salvar"),
-			@ApiResponse(code = 400, message = "Erro no body") })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema salvar"),})
 	@PostMapping("/salvar")
 	public ResponseEntity<TemaModel> novoTema(@Valid @RequestBody TemaModel novoTema) {
-		return ResponseEntity.status(201).body(repositorio.save(novoTema));
-
+		return ResponseEntity.status(201).body(repository.save(novoTema));
 	}
 
 	@ApiOperation(value = "Atualizar um tema")
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema atualizado"),
-			@ApiResponse(code = 204, message = "Não existe esse tema no sistema") })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema atualizado"),})
 	@PutMapping("/atualizar")
 	public ResponseEntity<TemaModel> alterarTema(@Valid @RequestBody TemaModel alterarTema) {
-		return ResponseEntity.status(200).body(repositorio.save(alterarTema));
+		return ResponseEntity.status(200).body(repository.save(alterarTema));
 
 	}
 
 	@ApiOperation(value = "Procura um tema por Id")
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema encontrado"),
-			@ApiResponse(code = 204, message = "Tema inexistente no sistema") })
-	@GetMapping("/{id}")
-	public ResponseEntity<TemaModel> findAllById(@Valid @PathVariable Long id) {
-		return repositorio.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
-				.orElse(ResponseEntity.status(204).build());
-	}
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Tema encontrado"),
+            @ApiResponse(code = 404, message = "Tema inexistente no sistema") })
+    @GetMapping("/pesquisar")
+    public ResponseEntity<TemaModel> findAllById(@Valid @RequestParam(defaultValue = "") Long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElseThrow(() -> {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tema inexistente");
+        });
+    }
 
 	@ApiOperation(value = "Excluir um tema")
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema deletada"),
-			@ApiResponse(code = 400, message = "Não existe esse tema no sistema") })
-	@DeleteMapping("/excluir/{id}")
-	public ResponseEntity<TemaModel> excluirTema(@Valid @PathVariable Long id) {
-		Optional<TemaModel> objetoOptional = repositorio.findById(id);
-		if (objetoOptional.isPresent()) {
-			repositorio.deleteById(id);
-			return ResponseEntity.status(200).build();
-		} else {
-			return ResponseEntity.status(400).build();
-		}
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Tema deletada"),
+            @ApiResponse(code = 404, message = "Não existe esse tema no sistema") })
+    @DeleteMapping("/excluir")
+    public ResponseEntity<Object> excluirTema(@Valid @RequestParam(defaultValue = "") Long id) {
+        return repository.findById(id).map(resp -> {
+            repository.deleteById(id);
+            return ResponseEntity.status(200).build();
+        }).orElseThrow(() -> {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tema inexistente");
+        });
 
-	}
+    }
 
 }
