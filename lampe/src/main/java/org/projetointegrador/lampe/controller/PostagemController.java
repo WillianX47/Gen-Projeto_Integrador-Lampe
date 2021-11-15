4b329e6f9c30e2770da5749a29c8673beeef4ab2
WillianX47 <willianx47@gmail.com>
2021-11-15 16:44:19 -0300

    [Feature Will - Task: hotfix | 15.11.21]

--- lampe/src/main/java/org/projetointegrador/lampe/controller/PostagemController.java
+++ lampe/src/main/java/org/projetointegrador/lampe/controller/PostagemController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -49,7 +48,7 @@ public class PostagemController {
 
 	@ApiOperation(value = "Procura uma postagem por ID")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Postagem encontrada"),
-			@ApiResponse(code = 204, message = "Postagem inexistente no sistema") })
+			@ApiResponse(code = 400, message = "Postagem inexistente no sistema") })
 	@GetMapping("/{id}")
 	public ResponseEntity<PostagemModel> findAllById(@Valid @PathVariable Long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.status(204).build());
@@ -65,33 +64,31 @@ public class PostagemController {
 
 	@ApiOperation(value = "Atualiza uma postagem")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Postagem atualizada"),
-			@ApiResponse(code = 204, message = "Não existe essa postagem no sistema") })
+			@ApiResponse(code = 400, message = "Não existe essa postagem no sistema") })
 	@PutMapping("/atualizar")
-	public ResponseEntity<PostagemModel> alterar(@Valid @RequestParam(defaultValue = "") Long id,
-			@RequestBody PostagemModel postagem) {
-		return repository.findById(id).map(resp -> {
+	public ResponseEntity<PostagemModel> alterar(@Valid @RequestBody PostagemModel postagem) {
+		return repository.findById(postagem.getId()).map(resp -> {
 			resp.setTextoPostagem(postagem.getTextoPostagem());
 			resp.setAnexoPostagem(postagem.getAnexoPostagem());
 			resp.setPrivacidade(postagem.getPrivacidade());
 			resp.setLocalizacaoPostagem(postagem.getLocalizacaoPostagem());
 			resp.setTemaPostagem(postagem.getTemaPostagem());
-			resp.setUsuarioPostagem(resp.getUsuarioPostagem());
 			return ResponseEntity.ok(repository.save(resp));
 		}).orElseThrow(() -> {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não existe uma postagem com esse id");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não existe uma postagem com esse id");
 		});
 	}
 
 	@ApiOperation(value = "Deleta uma postagem")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Postagem deletada"),
-			@ApiResponse(code = 404, message = "Não existe essa postagem no sistema") })
-	@DeleteMapping("/excluir")
-	public ResponseEntity<Object> excluir(@Valid @RequestParam(defaultValue = "") Long id) {
+			@ApiResponse(code = 400, message = "Não existe essa postagem no sistema") })
+	@DeleteMapping("/excluir/{id}")
+	public ResponseEntity<Object> excluir(@Valid @PathVariable Long id) {
 		return repository.findById(id).map(resp -> {
 			repository.deleteById(id);
 			return ResponseEntity.status(200).build();
 		}).orElseThrow(() -> {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não existe uma postagem com esse id");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não existe uma postagem com esse id");
 		});
 	}
 
--- lampe/src/main/java/org/projetointegrador/lampe/controller/TemaController.java
+++ lampe/src/main/java/org/projetointegrador/lampe/controller/TemaController.java
@@ -12,11 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -46,6 +46,16 @@ public class TemaController {
 		}
 	}
 
+	@ApiOperation(value = "Procura um tema por Id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema encontrado"),
+			@ApiResponse(code = 404, message = "Tema inexistente no sistema") })
+	@GetMapping("/pesquisar/{id}")
+	public ResponseEntity<TemaModel> findAllById(@Valid @PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElseThrow(() -> {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tema inexistente");
+		});
+	}
+
 	@ApiOperation(value = "Salvar um tema")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema salvar"), })
 	@PostMapping("/salvar")
@@ -58,24 +68,13 @@ public class TemaController {
 	@PutMapping("/atualizar")
 	public ResponseEntity<TemaModel> alterarTema(@Valid @RequestBody TemaModel alterarTema) {
 		return ResponseEntity.status(200).body(repository.save(alterarTema));
-
-	}
-
-	@ApiOperation(value = "Procura um tema por Id")
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema encontrado"),
-			@ApiResponse(code = 404, message = "Tema inexistente no sistema") })
-	@GetMapping("/pesquisar")
-	public ResponseEntity<TemaModel> findAllById(@Valid @RequestParam(defaultValue = "") Long id) {
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElseThrow(() -> {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tema inexistente");
-		});
 	}
 
 	@ApiOperation(value = "Excluir um tema")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Tema deletada"),
 			@ApiResponse(code = 404, message = "Não existe esse tema no sistema") })
 	@DeleteMapping("/excluir")
-	public ResponseEntity<Object> excluirTema(@Valid @RequestParam(defaultValue = "") Long id) {
+	public ResponseEntity<Object> excluirTema(@Valid @PathVariable Long id) {
 		return repository.findById(id).map(resp -> {
 			repository.deleteById(id);
 			return ResponseEntity.status(200).build();
